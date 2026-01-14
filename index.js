b50d619055dd7d2aa28aaeeb70d57abb13302f4f
somildholakia <somildholakia0852@gmail.com>
2026-01-14 12:26:19 +0530

    update

--- index.js
+++ index.js
@@ -1,77 +1,47 @@
-const express = require("express");
+const express = require("express")
 const app = express();
 const path = require("path");
-
-const {v4: uuid, v4} = require("uuid");
-
+// let { uuid:v4 } = require("uuid");
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
-
-const port = 8080;
-app.set("view engine","ejs");
+app.set("view engine", "ejs");
 app.set("views",path.join(__dirname,"views"));
 app.use(express.static(path.join(__dirname,"public")));
 
 
-app.listen(port,()=> {
-    console.log(`Listening at port: ${port}`);
-})
-
 let posts = [
     {
-        id: uuid(v4),
-        username: "somil",
-        content: "I love coding",
+        // id: uuid(v4),
+        user: "Somil",
+        content: "I like coding",
     },
     {
-        id: uuid(v4),
-        username: "ritik",
-        content: "I love coding",
+        // id: uuid(v4),
+        user: "ritik",
+        content: "I like coding",
     },
     {
-        id: uuid(v4),
-        username: "vivek",
-        content: "I love coding",
+        // id: uuid(v4),
+        user: "krishna",
+        content: "I like coding",
     },
     {
-        id: uuid(v4),
-        username: "krishna",
-        content: "I love coding",
+        // id: uuid(v4),
+        user: "vivek",
+        content: "I like coding",
     },
+]
 
-];
+
+const port = 8080;
+
+app.listen(port, () => {
+    console.log(`Listening at port:${port}`)
+})
 
 app.get("/posts", (req,res) => {
-    res.render("index.ejs",{posts})
+    res.render("index.ejs",{ posts });
 })
 
-app.get("/posts/new", (req,res) => {
-    res.render("new.ejs")
-})
-
-app.post("/posts", (req,res) => {
-    let {username,content} = req.body;
-    let id = uuid(v4);
-    posts.push({ id,username, content})
-    res.redirect("/posts")
-})
-
-app.get("/posts/:id", (req,res) => {
-    let {id} = req.params
-    let post = posts.find((p) => id === p.id)
-    console.log(post)
-    res.render("show.ejs", {post})
-})
-
-app.patch("/posts/:id",(req,res) => {
-    let { id  } = req.params;
-    let newcontent = req.body.content
-    let post = posts.find((p) => id === p.id)
-    post.content = newcontent;
-    console.log(post)
-   
-
-    res.send("Patch request working")
-})
